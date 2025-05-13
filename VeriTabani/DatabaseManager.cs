cf9bd8e7624ffcfa6d23aed70bc9f9a5b61fe67e
Yoru <yorunoken@ik.me>
2025-05-13 22:07:12 +0300

    Rename folders

---
+++ VeriTabani/DatabaseManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet_Kafe_Proje.Veri_Tabanı
+{
+    public class DatabaseManager
+    {
+
+    }
+}
