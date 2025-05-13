173620989ec0aca16c53de45e1c6a95adff63eb7
Yoru <Yoru@DESKTOP-HPN5B2O>
2025-05-13 21:48:04 +0300

    Add project files.

---
+++ GirisForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Internet_Kafe_Proje
+{
+    partial class GirisForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label labelTitle;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.panelLogin.SuspendLayout();
+            this.SuspendLayout();
+
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelLogin.BackColor = System.Drawing.Color.White;
+            this.panelLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelLogin.Controls.Add(this.labelTitle);
+            this.panelLogin.Controls.Add(this.labelUsername);
+            this.panelLogin.Controls.Add(this.textBoxUsername);
+            this.panelLogin.Controls.Add(this.labelPassword);
+            this.panelLogin.Controls.Add(this.textBoxPassword);
+            this.panelLogin.Controls.Add(this.buttonLogin);
+            this.panelLogin.Location = new System.Drawing.Point(250, 100);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(300, 280);
+            this.panelLogin.TabIndex = 0;
+
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.labelTitle.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.labelTitle.Location = new System.Drawing.Point(0, 15);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(300, 40);
+            this.labelTitle.Text = "Welcome Back";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.labelUsername.Location = new System.Drawing.Point(20, 65);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(260, 20);
+            this.labelUsername.Text = "Username";
+
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.textBoxUsername.Location = new System.Drawing.Point(20, 85);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(260, 25);
+            this.textBoxUsername.TabIndex = 1;
+
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.labelPassword.Location = new System.Drawing.Point(20, 120);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(260, 20);
+            this.labelPassword.Text = "Password";
+
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.textBoxPassword.Location = new System.Drawing.Point(20, 140);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(260, 25);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.buttonLogin.BackColor = System.Drawing.Color.Teal;
+            this.buttonLogin.ForeColor = System.Drawing.Color.White;
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogin.Location = new System.Drawing.Point(20, 190);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(260, 35);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "Sign In";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelLogin);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ GirisForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Internet_Kafe_Proje
+{
+    public partial class GirisForm: Form
+    {
+        public GirisForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
