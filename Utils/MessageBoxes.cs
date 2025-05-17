f3d6de7108192d767a76ac37b0fcfb11e2519616
Yoru <yorunoken@ik.me>
2025-05-17 16:54:37 +0300

    Mesaj kutusu için kısayol metodu

---
+++ Utils/MessageBoxes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet_Kafe_Proje.Utils
+{
+    class MessageBoxes
+    {
+        static internal void ErrorBox(string errorDesc)
+        {
+            MessageBox.Show(errorDesc, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
